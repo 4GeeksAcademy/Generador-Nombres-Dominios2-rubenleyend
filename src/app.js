36167e09d0cb3db6341b4e577b1d093a44a7a019
Rubenleyend <rubenmartinezamez@gmail.com>
2025-07-25 11:05:27 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the', 'our'];
+let adjetive = ['great', 'big'];
+let noun = ['jogger', 'puedes'];
+let extension = [".com", ".net", ".us", ".es"];
+window.onload = function () {
+
+  document.getElementById("generador").innerHTML = generadorDominios().join("<br>")
+
+  function generadorDominios() {
+    let dominios = [];
+    for (let pro of pronoun) {
+      for (let adj of adjetive) {
+        for (let nom of noun) {
+          for (let ext of extension) {
+            if (nom === "puedes" && ext === ".es") {
+           
+              dominios.push(`${pro}${adj}pued${ext}`)
+
+            } else {
+
+              dominios.push(`${pro}${adj}${nom}${ext}`)
+            }
+
+          }
+        }
+
+      }
+    }
+    return dominios;
+
+  }
+
 };
